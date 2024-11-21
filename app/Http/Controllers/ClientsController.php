84073ea0900c82f915bc01caed3c19fcc7874ce3
Davi Mancebo <87788044+Davi-Mancebo@users.noreply.github.com>
2024-11-20 23:16:10 -0300

    just removing white spaces

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -31,35 +31,21 @@ class ClientsController extends Controller
 
     public function update(UpdateClientRequest $updateClientRequest, $id)
     {
-        // Encontrar o cliente pelo ID
         $client = Clients::find($id);
-
-        // Se o cliente não existir
         if (!$client) {
             return response()->json(['message' => 'Client not found'], Response::HTTP_NOT_FOUND);
         }
-
-        // Atualizar o cliente com os dados validados
         $client->update($updateClientRequest->validated());
-
-        // Retornar o cliente atualizado
         return response()->json($client, Response::HTTP_OK);
     }
 
     public function destroy($id)
     {
-        // Encontrar o cliente
         $client = Clients::find($id);
-
-        // Se o cliente não existir
         if (!$client) {
             return response()->json(['message' => 'Client not found'], Response::HTTP_NOT_FOUND);
         }
-
-        // Excluir o cliente
         $client->delete();
-
-        // Retornar resposta de sucesso
         return response()->json(['message' => 'Client deleted successfully'], Response::HTTP_NO_CONTENT);
     }
 }
